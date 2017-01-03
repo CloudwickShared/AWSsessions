3808cb82641f11f48b0c42e6fd67f6215a720228
Derwin McGeary <derwin.mcgeary@cloudwick.com>
2017-01-03 23:25:13 +0000

    Include files in the repository

--- setup-aws.sh
+++ setup-aws.sh
@@ -3,30 +3,13 @@
 # This script should prepare your EC2 instance for the AWS tutorial
 # assuming instance type ami-9398d3e0
 
-sudo yum -y update
-echo "Installing developer tools..."
+yum -y update
 yum -y install epel-release
 
-# sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-# sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install java-1.7.0-openjdk
-sudo yum install -y apache-maven gcc # build tools
-sudo yum install -y git emacs vim # hacking tools
-sudo yum install -y aws-kinesis-agent
-# echo "Downloading aws java sdk..."
-# wget https://sdk-for-java.amazonwebservices.com/latest/aws-java-sdk.zip
-# unzip aws-java-sdk.zip
-# echo "Done"
+yum install -y java-1.7.0-openjdk
+yum install -y apache-maven gcc # build tools
+yum install -y git emacs vim # hacking tools
+yum install -y aws-kinesis-agent
 
-echo "Test: Downloading and compiling AWS sample app"
-git clone https://github.com/awslabs/aws-java-sample.git
-cd aws-java-sample
-mvn package
-cd ..
-
-echo "installing requirements for Fake Log generator"
-sudo pip install fake-factory numpy pytz
+pip install fake-factory numpy pytz
 git clone https://github.com/kiritbasu/Fake-Apache-Log-Generator.git
-
-echo "Run: cd Fake Apache-Log-Generator&& python apache-fake-log-gen.py -o LOG -n 0"
-echo "When you want to generate infinitely long fake logs"
